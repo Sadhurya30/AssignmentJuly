13f0573da9e029069185ff2b80a50a31ea18a1fd
Sadhurya30 <132685899+Sadhurya30@users.noreply.github.com>
2023-07-30 22:09:50 +0530

    Add files via upload

---
+++ SortTheValues.java
@@ -0,0 +1,19 @@
+package july.assignment;
+
+import java.util.Arrays;
+
+public class SortTheValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         //declare an integer array and assign the values
+		 int[] numbers = {10,5,7,9,8,6};
+		 
+		 // sort the array
+		 Arrays.sort(numbers);
+		 
+		 //Convert the arrays to string and print the string.
+		 System.out.println(Arrays.toString(numbers));
+	}
+
+}
