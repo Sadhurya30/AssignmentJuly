b83094e25d47cb02bb0dc2bc7a6b04e3ac7da141
Sadhurya30 <132685899+Sadhurya30@users.noreply.github.com>
2023-07-30 22:09:17 +0530

    Add files via upload

---
+++ SecondLaargestNumber.java
@@ -0,0 +1,17 @@
+package july.assignment;
+
+public class SecondLaargestNumber {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			//declare an integer array and assign the values
+			  int[] num = {1,2,3,4,5,6,7,8,9};
+			 // declare a variable len and get the length of the array
+			  int len = num.length;
+			 // print the second largest element
+			  System.out.println(num[len-2]);
+			  
+		}
+
+	}
